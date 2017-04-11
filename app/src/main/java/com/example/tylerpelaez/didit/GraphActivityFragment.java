869a1a431da06b0ced339f96e34714c13546f264
Matthew <chuangmatthew@yahoo.com>
2017-04-10 22:03:07 -0400

    Add empty GraphActivity and GraphActivityFragment

---
+++ app/src/main/java/com/example/tylerpelaez/didit/GraphActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.tylerpelaez.didit;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class GraphActivityFragment extends Fragment {
+
+    public GraphActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_graph, container, false);
+    }
+}
